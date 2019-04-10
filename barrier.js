953152574049e806b686ac9b569a62e6ac54a6bd
ZhengChen He <31530891+HHeOC@users.noreply.github.com>
2019-04-10 11:37:29 -1000

    Add files via upload

--- barrier.js
+++ barrier.js
@@ -2,6 +2,7 @@ var w = window.innerWidth;
 var h = window.innerHeight;
 var barrier = new barrier();
 var miss = 0;
+var en;
 function barrier(){
         this.x = w;
         this.y = Math.floor(Math.random()*h);
@@ -10,7 +11,7 @@ function barrier(){
         this.velocity = 0;
         this.show = function(){
           fill(color('white'));
-          rect(this.x+100, this.y, 120,this.yrad);
+          image(en,this.x+100, this.y, 120,this.yrad);
         }
         this.update = function(){
           this.velocity += this.gravity; //gravity applied when not jumping
